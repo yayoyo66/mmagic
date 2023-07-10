eca04abd5a218fab4eb2ca0fa9b2ce5ea7b2bec9
yayoyo66 <xieziqi42@126.com>
2023-07-10 19:48:07 +0800

    update launch

---
+++ projects/QRcode_generator/launch.py
@@ -0,0 +1,125 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+
+import cv2
+import numpy as np
+import mmcv
+from mmengine import Config
+from PIL import Image
+import os
+import pyqrcode
+import io
+import torch
+
+from mmagic.registry import MODELS
+from mmagic.utils import register_all_modules
+import gradio as gr
+from mmengine.logging import MMLogger
+
+
+
+class qrcode():
+
+    def __init__(self) -> None:
+        self.control = None
+        self.generated_img = None
+
+
+    def change(self, ratio=0.5):
+        w = int(self.generated_img.shape[1] * ratio)
+        c1 = self.control[:,:w,:]
+        c2 = self.generated_img[:,w:,:]
+        com_img = np.concatenate([c1, c2], axis=1)
+        return com_img
+
+    def composite(self, qrcode, generated_img, ratio=0.5):
+        w = int(generated_img.shape[1] * ratio)
+        c1 = qrcode[:,:w,:]
+        c2 = generated_img[:,w:,:]
+        com_img = np.concatenate([c1, c2], axis=1)
+        return com_img
+    
+    def qrcode_generator(self,
+                    text, 
+                    prompt, 
+                    negative_prompt,
+                    num_inference_steps,
+                    guidance_scale,
+                    controlnet_conditioning_scale
+                    ):
+    
+        qr= pyqrcode.create(text, error='H', version=3, mode='binary')
+        qr.png('qrcode.png', scale=15)
+
+        cfg = Config.fromfile('controlnet-brightness.py')
+        cfg.model.unet.from_pretrained = 'dreamlike-art/dreamlike-diffusion-1.0'
+        cfg.model.vae.from_pretrained = 'dreamlike-art/dreamlike-diffusion-1.0'
+        cfg.model.controlnet.from_pretrained = 'ioclab/control_v1p_sd15_brightness'
+
+        cfg.model.init_cfg['type'] = 'convert_from_unet'
+
+        controlnet = MODELS.build(cfg.model).cuda()
+
+        # call init_weights manually to convert weight
+        controlnet.init_weights()
+
+        prompt =  prompt
+        negative_prompt = negative_prompt
+        control_img = mmcv.imread('qrcode.png')
+        control_img = cv2.resize(control_img, (512,512))
+        control_img = control_img[:,:,0:1]
+        control_img = np.concatenate([control_img]*3, axis=2)
+        control = Image.fromarray(control_img)
+
+        num_inference_steps = num_inference_steps
+        guidance_scale = guidance_scale
+        num_images_per_prompt = 1
+        controlnet_conditioning_scale = controlnet_conditioning_scale
+        height=512
+        width=512
+        
+        output_dict = controlnet.infer(
+                        prompt = prompt, 
+                        control = control,
+                        height = height,
+                        width = width,
+                        controlnet_conditioning_scale=controlnet_conditioning_scale,
+                        num_inference_steps=num_inference_steps, 
+                        guidance_scale=guidance_scale,
+                        num_images_per_prompt=num_images_per_prompt,
+                        negative_prompt=negative_prompt,
+                        )
+
+        generated_img  = output_dict['samples'][0]
+        self.control = np.array(control)
+        self.generated_img = np.array(generated_img)
+        com_img = self.composite(self.control, self.generated_img)
+    
+        return com_img
+    
+
+if __name__ == '__main__':
+    
+    register_all_modules()
+    generator = qrcode()
+
+    with gr.Blocks() as demo:
+        gr.Markdown('# MMagic QRcode generator Demo')
+        gr.Markdown('This is a QRcode generator based on MMagic')
+        text = gr.Textbox(label="QRcode text", placeholder='test')
+        prompt= gr.Textbox(label="Prompt",placeholder='dreamlikeart, an zebra')
+        negative_prompt= gr.Textbox(label="Negative Prompt",placeholder='ugly, bad quality')
+        with gr.Row():
+            num_inference_steps = gr.Slider(0, 100, value=50, step=1, label="Inference Steps")
+            guidance_scale = gr.Slider(0, 10, value=7.5, step=0.5, label="Guidance Scale")
+            controlnet_conditioning_scale = gr.Slider(0, 1, value=0.7, label="Controlnet Conditioning Scale")
+        run_btn = gr.Button("Run")
+        ratio = gr.Slider(0, 1, value=0.5, step=0.1)
+        with gr.Row():
+            o = gr.Image(shape=(256, 256))
+        inputs = [text,prompt,negative_prompt,num_inference_steps,guidance_scale,controlnet_conditioning_scale]
+        run_btn.click(fn= generator.qrcode_generator, inputs=inputs, outputs=o)
+        ratio.change(fn = generator.change, inputs = ratio, outputs = o)
+
+    demo.launch()
+    
+
